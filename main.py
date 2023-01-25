7dd7762d256b01ef734c265abea3dec9086b0a49
“Vitali <abdullajondev@gmail.com>
2023-01-25 22:52:21 +0500

    added new changes

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
-user_input = input("please enter your password: ")
-print(user_input)
-print((len(user_input)))
+password = input("please enter your password: ")
+
+digits = {}
+
+if len(password) >= 8:
+    digits["length"] = True
+else:
+    digits["length"] = False
+
+digit = False
+for i in password:
+    if i.isdigit():
+        digit = True
+digits["digit"] = digit
+
+
+uppercase = False
+for i in password:
+    if i.isupper():
+        uppercase = True
+
+digits["uppercase"] = uppercase
+
+
+print((digits))
+if all(digits.values()) == True:
+    print("strong password")
+else:
+    print("weak password")
